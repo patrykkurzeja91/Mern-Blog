162844728c05457f938c2ba0c5e8d1795011ff57
Patryk <patrick.kurzeja@gmail.com>
2018-07-15 13:01:15 +0200

    Deploy

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://<patraspan>:<mongodbPass123456789>@ds161780.mlab.com:61780/mern-blog' || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://patraspan:mongodbPass123456789@ds161780.mlab.com:61780/mern-blog' || 'mongodb://localhost:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
